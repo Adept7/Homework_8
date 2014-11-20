803ef63d892e7da53f93e1478ea254645cbe9583
Jack Dubil <jackdubil@gmail.com>
2014-11-20 15:24:06 -0800

    Initial checkin after formatting and things.

---
+++ src/homework8/Potion.java
@@ -0,0 +1,23 @@
+package homework8;
+
+public class Potion
+{
+	private double Size;
+	private int Type; // 0, 1, 2, 3 ... etc. A flag value.
+
+	public Potion(double NewSize, int NewType)
+	{
+		Size = NewSize;
+		Type = NewType;
+	}
+
+	/*
+	 * YOUR CODE HERE
+	 * Override the hashCode method.
+	 */
+
+	/*
+	 * YOUR CODE HERE
+	 * Override the equals method.
+	 */
+}
---
+++ src/homework8/Weapon.java
@@ -0,0 +1,25 @@
+package homework8;
+
+public class Weapon
+{
+	private double Damage;
+	private double AttackSpeed;
+	private double MissChance;
+
+	public Weapon(double NewDamage, double NewAttackSpeed, double NewMissChance)
+	{
+		Damage = NewDamage;
+		AttackSpeed = NewAttackSpeed;
+		MissChance = NewMissChance;
+	}
+
+	/*
+	 * YOUR CODE HERE
+	 * Override the hashCode method. 
+	 */
+
+	/*
+	 * YOUR CODE HERE
+	 * Override the equals method.
+	 */
+}
